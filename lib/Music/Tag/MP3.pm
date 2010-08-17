7fb59c3e11230016ef0fc0eb323d7f6e31237a93
Edward Allen <ealleniii@cpan.org>
2010-08-16 20:39:06 -0700

    Version 0.4101

--- lib/Music/Tag/MP3.pm
+++ lib/Music/Tag/MP3.pm
@@ -1,6 +1,6 @@
 package Music::Tag::MP3;
 use strict; use warnings; use utf8;
-our $VERSION = '.4101';
+our $VERSION = '0.4101';
 
 # Copyright © 2007,2010 Edward Allen III. Some rights reserved.
 #
